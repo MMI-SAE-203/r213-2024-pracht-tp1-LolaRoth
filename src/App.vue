9e13b1f12e6c3ed488e9a37949f8f432ea1038e7
Lola Roth <lola.roth@edu.univ-fcomte.fr>
2024-04-11 13:46:11 +0200

    ajt de la ref dans le script apres test devTools

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { onErrorCaptured } from 'vue'
 import { RouterLink, RouterView } from 'vue-router/auto'
+import {ref} from 'vue'
+const menuIsOpen = ref(false)
+
 
 onErrorCaptured((err, instance, info) => {
   console.error('erreur : ', err, '\ninfo : ', info, '\ncomposant : ', instance)
